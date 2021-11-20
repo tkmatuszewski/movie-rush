526a803b5be6c8eb8bed56cf2e8fe0b65e61d30d
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-11-20 20:54:41 +0100

    project config

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+
+function App() {
+  return (
+    <div className="App">
+      <createGlobalStyle>
+        <h1>App</h1>
+      </createGlobalStyle>
+    </div>
+  );
+}
+
+export default App;
