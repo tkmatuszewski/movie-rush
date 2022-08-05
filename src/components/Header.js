b58d0b009a35e1cb0b2b7a7c247616a26910e986
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-08-05 10:59:37 +0200

    Header styling switched to grid

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,29 @@
 import styled from "styled-components";
 
 const HeaderStyled = styled.header`
-  height: ${(props) => props.height};
   display: flex;
   justify-content: center;
   align-items: center;
   background: black;
-  
+
   .header__container {
-  width: 90%;
-  height: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    width: 90%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    padding: 2vh 0;
 
     a {
       text-decoration: none;
-      width: 25%;
+      @media only screen and (max-width: 665px) {
+        display: none;
+      }
     }
   }
 `;
 
-export const Header = ({children, headerHeight}) => {
+export const Header = ({children}) => {
   return (
-    <HeaderStyled height={headerHeight}>
+    <HeaderStyled>
       <div className="header__container">
         {children}
       </div>
