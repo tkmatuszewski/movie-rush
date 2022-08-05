e65bb8fc5d1e4c1c1dd1329037e01564e85a658c
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-08-05 11:11:16 +0200

    Movie redesigned and refactored, responsive stying

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,149 +1,119 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense, lazy } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { Loader } from "./Loader";
+import { Facts } from './Facts';
+import { Reviews } from "./Reviews";
+const Credits = lazy(() => import('./Credits'));
 
 const MovieStyled = styled.div`
-  font-family: "Lato", sans-serif;
   margin-bottom: 5%;
 
-  .movie__cnt {
-    width: 70%;
-    margin: 0 auto;
+  .movie {
     display: grid;
-    grid-template-rows: repeat(auto, 4);
-    grid-row-gap: 3%;
+    grid-template-rows: repeat(4, auto);
     grid-template-columns: 1fr 1fr;
+    margin-top: 5vh;
+    @media only screen and (max-width: 768px) {
+     margin-top: 0;
+    }
+    
+    .box {
+      padding: 5vw;
+    }
+
+    h4 {
+      font-family: "Oswald", sans-serif;
+      font-size: 2rem;
+      margin-top: 0;
+      margin-bottom: 1rem;
+    }
   }
 
   .movie__wrapper {
     padding: 8%;
   }
 
-  .movie__head {
+  .movie__hero {
+    grid-row: 1/2;
     grid-column: 1/3;
-    grid-row: 1/2;
     display: grid;
-    grid-template-columns: 2fr 2fr 1fr;
-    grid-template-rows: repeat(3, 1fr);
-    color: black;
-    border-bottom: 0.25px solid gray;
-  }
-
-  .movie__title {
-    grid-column: 1/2;
-    grid-row: 1/4;
-    font-size: 3rem;
-  }
-
-  .movie__genre {
-    grid-column: 2/3;
-    grid-row: 1/2;
-    justify-self: flex-end;
-    align-self: flex-end;
-    opacity: 0.6;
-  }
-
-  .movie__year {
-    grid-column: 2/3;
-    grid-row: 2/3;
-    opacity: 0.6;
-    justify-self: flex-end;
-    align-self: center;
-  }
-
-  .movie__runtime {
-    grid-column: 2/3;
-    grid-row: 3/4;
-    justify-self: flex-end;
-  }
-
-  .movie__r {
-    grid-column: 3/4;
-    grid-row: 1/4;
-    font-size: 2.5rem;
-    opacity: 0.3;
-    align-self: center;
+    grid-template-rows: max-content;
     justify-self: center;
+    color: white;
+    margin-bottom: 5vh;
+    @media only screen and (max-width: 768px) {
+      margin-bottom: 0;
+    }
+
+    .movie__img {
+      grid-row: 1/3;
+      grid-column: 1/3;
+      width: 80vw;
+
+      @media only screen and (max-width: 768px) {
+        width: 100%;
+      }
+    }
+
+    .movie__hero__data {
+      grid-row: 1/3;
+      grid-column: 1/3;
+      padding: 1rem;
+      background: linear-gradient(180deg, #00000040, #ffffff00);
+
+      .movie__title {
+        font-size: clamp(1.5rem, 2vh, 3rem);
+        margin: 1rem 0;
+      }
+
+      .movie__genres,
+      .movie__runtime {
+        font-size: clamp(1rem, 2vh, 1.5rem);
+        line-height: 2rem;
+      }
+    }
+  }
+
+  .movie__rating {
+    grid-column: 2/3;
+    grid-row: 1/4;
+    justify-self: flex-end;
+    align-self: flex-start;
+    font-size: clamp(1.2rem, 2vh, 3rem);
+    background: black;
+    padding: 1.5rem;
+
+    span {
+      display: block;
+    }
+
+    .movie__count {
+      grid-column: 2/3;
+      grid-row: 2/3;
+      font-size: 2.5rem;
+      justify-self: flex-end;
+      font-size: 3rem;
+    }
   }
 
   .movie__data {
     grid-row: 3/4;
     grid-column: 1/2;
-    /* min-height: 60vh; */
     background: rgba(0, 0, 0, 0.9);
     color: white;
   }
 
-  .movie__data__title {
-    font-size: 2rem;
-    margin-top: 0;
-    margin-bottom: 1rem;
-  }
-
-  .movie__line {
-    line-height: 1.5;
-    opacity: 0.9;
-  }
-
-  .movie__img {
-    grid-row: 2/4;
-    grid-column: 2/3;
-    align-self: center;
-    justify-self: center;
-    /* transform: translate(-10%, 10%); */
-  }
-
   .movie__plot {
-    grid-row: 2/3;
-    grid-column: 1/2;
-    text-indent: 10%;
-    line-height: 1.2;
-    align-self: center;
-  }
-
-  .movie__ratings {
-    width: 100%;
     grid-row: 4/5;
-    grid-column: 1/3;
-    justify-self: flex-end;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    margin-bottom: 5%;
-
-    span {
-      width: 50%;
-      line-height: 1.5;
-    }
-  }
-  .movie__ratings__title {
-    font-size: 2rem;
-    margin-bottom: 1.5rem;
-    display: block;
-    width: 100%;
-    padding-bottom: 1rem;
-    border-bottom: 1px solid gray;
-  }
-
-  .movie__ratings__line {
-    width: 30%;
-    display: flex;
-    justify-content: space-between;
-    font-weight: bold;
-    opacity: 0.5;
-    font-size: 1.2rem;
-  }
-
-  .movie__imdb {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    strong {
-      font-size: 3rem;
-      color: gray;
+    grid-column: 2/3;
+    line-height: 1.2;
+    font-size: 1.5rem;
+    border-bottom: 1px solid black;
+     @media only screen and (max-width: 768px) {
+       grid-row: 2/3;
+       grid-column: 1/3;
     }
   }
 `;
@@ -158,7 +128,7 @@ export const Movie = () => {
     const axiosData = async () => {
       try {
         const { data } = await axios.get(
-          `http://www.omdbapi.com/?i=${movieId}&apikey=a7843424&plot=full`
+          `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.REACT_APP_API_KEY}`
         );
         setMovie(data);
       } catch (error) {
@@ -169,44 +139,48 @@ export const Movie = () => {
   }, [movieId]);
 
   return (
-      <MovieStyled>
-        {movie ? (
-          <div className="movie__cnt">
-            <div className="movie__head">
-              <h1 className="movie__title">{movie.Title}</h1>
-              <strong className="movie__genre">{movie.Genre}</strong>
-              <strong className="movie__year">{movie.Year}</strong>
-              <span className="movie__runtime">{movie.Runtime}</span>
-              <span className="movie__r">{movie.imdbRating}</span>
+    <MovieStyled>
+      {error && <div>{error}</div>}
+      {movie ? (
+        <div className="movie">
+          <div className="movie__hero">
+            <img
+              className="movie__img"
+              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+              alt="Movie poster"
+            />
+            <div className="movie__hero__data">
+              <h2 className="movie__title">
+                {movie.title + " (" + movie.release_date.split("-")[0] + ")"}
+              </h2>
+              <div className="movie__genres">
+                {movie.genres.map((el, index) => (
+                  <span key={index}>
+                    {index ? ", " : ""}
+                    {el.name}
+                  </span>
+                ))}
+              </div>
+              <span className="movie__runtime">{movie.runtime}min</span>
             </div>
-            <div className="movie__plot movie__wrapper">{movie.Plot}</div>
-            <img className="movie__img" src={movie.Poster} />
-            <div className="movie__data movie__wrapper">
-              <h4 className="movie__data__title">Facts</h4>
-              <div className="movie__line">Director: {movie.Director}</div>
-              <div className="movie__line"> Cast: {movie.Actors}</div>
-              <div className="movie__line"> Writer: {movie.Writer}</div>
-              <div className="movie__line">Release date: {movie.Released}</div>
-              <div className="movie__line"> Production: {movie.Country}</div>
-              <div className="movie__line"> Box Office: {movie.BoxOffice}</div>
-              <div className="movie__line"> Awards: {movie.Awards}</div>
-              <div className="movie__line"> Metascore: {movie.Metascore}</div>
-            </div>
-            <div className="movie__ratings">
-              <h4 className="movie__ratings__title">Ratings</h4>
-              {movie.Ratings.map(({ Source, Value }) => {
-                return (
-                  <div className="movie__ratings__line" key={Source}>
-                    <span>{Source}</span>
-                    <span>{Value}</span>
-                  </div>
-                );
-              })}
+            <div className="movie__rating">
+              <span>{movie.vote_average}</span>
+              <span>({movie.vote_count}) </span>
             </div>
           </div>
-        ) : (
-          <Loader/>
-        )}
-      </MovieStyled>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Credits movieId={movie.id} />
+          </Suspense>
+          <Facts movie={movie} />
+          <div className="movie__plot box">
+            <h4>Plot</h4>
+            <p>{movie.overview}</p>
+          </div>
+          <Reviews movieId={movie.id} />
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </MovieStyled>
   );
 };
