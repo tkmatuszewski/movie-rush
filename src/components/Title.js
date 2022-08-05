7a00f19eefccaf32beed0be8c21794f878ebb983
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-08-05 11:02:39 +0200

    Title responsive

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,30 +1,19 @@
 import styled from "styled-components";
 
 const TitleStyled = styled.div`
-  width: 100%;
   font-size: clamp(1.5rem, 5vw, 2.5rem);
   font-weight: 900;
   font-family: "Oswald", sans-serif;
   color: white;
-  display: flex;
-  justify-content: space-between;
   text-transform: uppercase;
-  line-height: 2rem;
+  letter-spacing: 5px;
 `;
-
-export const Title = ()=> {
-    return (
-        <TitleStyled>
-            <span>M</span>
-            <span>o</span>
-            <span>v</span>
-            <span>i</span>
-            <span>e</span>
-            <span>r</span>
-            <span>u</span>
-            <span>s</span>
-            <span>h</span>
-        </TitleStyled>
-    )
-}
-
+export const Title = () => {
+  return (
+    <>
+      <TitleStyled>
+        Movierush
+      </TitleStyled>
+    </>
+  );
+};
